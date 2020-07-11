9ed2235b56bec5f7324d5d60d3bef6818af4d49f
EvanG <evangdesigns@gmail.com>
2020-07-10 21:01:02 -0500

    created api controller

---
+++ EmployerTerminal/Models/Skill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace EmployerTerminal.Models
+{
+    public class Skill
+    {
+        public int Skill_id { get; set; }
+        public int Skill_categroy { get; set; }
+        public string Skill_name { get; set; }
+        public string Icon_svg { get; set; }
+    }
+}
---
+++ EmployerTerminal/Models/TitleSkills.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace EmployerTerminal.Models
+{
+    public class TitleSkills
+    {
+        public int Title_skills_id { get; set; }
+        public int Title__id { get; set; }
+        public int Skills_id { get; set; }
+    }
+}
