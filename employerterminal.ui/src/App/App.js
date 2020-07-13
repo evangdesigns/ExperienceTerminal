df38a1341e00a3790227c789f72308202766ff5d
EvanG <evangdesigns@gmail.com>
2020-07-12 21:31:25 -0500

    implemented react app

--- employerterminal.ui/src/App/App.js
+++ employerterminal.ui/src/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hey Girl</h1>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
