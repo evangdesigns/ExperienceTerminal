43906b0882dc05374b1507a8b419e64d2f8999ab
EvanG <evangdesigns@gmail.com>
2020-10-19 22:47:31 -0500

    wip

--- employerterminal.ui/src/Components/Shared/Project/Project.js
+++ employerterminal.ui/src/Components/Shared/Project/Project.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import { ReactComponent as URLico} from '../../../images/icons/icon_website.svg';
+import { ReactComponent as GITico} from '../../../images/icons/icon_GitHub.svg';
 import { getImagesByProjectId } from '../../../helpers/data/images';
 import './Project.scss';
 
@@ -9,42 +11,62 @@ class Project extends React.Component {
   }
 
   componentDidMount() {
-    const { projectId } = this.props;
-    getImagesByProjectId(projectId)
+    const { project } = this.props;
+    getImagesByProjectId(project.project_id)
       .then(images => this.setState({ images : images }));
+  }
+
+  sliderRender = () => {
+    const { images } = this.state;
+    const { project } = this.props;
+    if (images && project.project_image_url) {
+      return (
+        <Carousel>
+          <Carousel.Item>
+            <img
+            className="carouselImage"
+            src={project.project_image_url}
+            alt={project.project_name}
+            />
+          </Carousel.Item>
+          {images.map(image => (
+            <Carousel.Item key={image.image_id}>
+              <img
+              className="carouselImage"
+              src={image.image_url}
+              alt={image.image_name}
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      )
+    } else {
+      return null;
+    }
   }
 
   render () {
     const { project } = this.props;
     const { images } = this.state;
     return (
-      <tr className="Project">
+      <>
+         <tr className="Project">
        <td colSpan="3">
-         {project.project_image_url !== ""?
-         <Carousel>
-         <Carousel.Item>
-           <img
-           className="d-block w-100"
-           src={project.project_image_url}
-           alt={project.project_name}
-           />
-         </Carousel.Item>
-         {images.map(image => (
-           <Carousel.Item key={image.image_id}>
-             <img
-             className="d-block w-100"
-             src={image.image_url}
-             alt={image.image_name}
-             />
-           </Carousel.Item>
-         ))}
-       </Carousel>
-         : null}
-          <div>
-            {project.project_description}
+          {this.sliderRender()}
+        </td>
+      </tr>
+      <tr className="Project">
+      <td colSpan="3">
+          <div className="projectDescription">
+            <p>{project.project_description}</p>
+          </div>
+          <div className="projectLinks">
+            {project.project_url ? <a href={project.project_url} rel="noopener noreferrer" target="_blank"><URLico/></a> : null}
+            {project.project_git_url ? <a href={project.project_git_url} rel="noopener noreferrer" target="_blank"><GITico/></a> : null}
           </div>
          </td>
       </tr>
+      </>
     );
   }
 }
