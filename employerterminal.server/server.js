eaa8bd36385304b50d053c64d1fbd00b12425463
EvanG <evangdesigns@gmail.com>
2021-01-19 21:08:01 -0600

    WIP

--- employerterminal.server/server.js
+++ employerterminal.server/server.js
@@ -9,10 +9,15 @@ if (port == null || port == "") {
   port = 8000;
 }
 app.listen(port);
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
   console.log("MongoDB database connection established successfully")
