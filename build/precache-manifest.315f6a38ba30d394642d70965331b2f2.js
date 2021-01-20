0459fd32d353c1310ff4c5901321bf49fc7370a1
EvanG <evangdesigns@gmail.com>
2021-01-20 15:42:17 -0600

    moved build

---
+++ build/precache-manifest.315f6a38ba30d394642d70965331b2f2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb726992603d476b701e52be7b818503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b02393a467dc4173dd2a",
+    "url": "/static/css/main.0c88ff29.chunk.css"
+  },
+  {
+    "revision": "aff7418522032302ef23",
+    "url": "/static/js/2.399c7c86.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.399c7c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02393a467dc4173dd2a",
+    "url": "/static/js/main.114cbf23.chunk.js"
+  },
+  {
+    "revision": "c4a1c9f11933fa46e2a5",
+    "url": "/static/js/runtime-main.ff137a4f.js"
+  },
+  {
+    "revision": "fc6791d7713921ade9aed5744b94dd5e",
+    "url": "/static/media/icon_Contact.fc6791d7.svg"
+  },
+  {
+    "revision": "174d45369283706f61ff9af2c03356e1",
+    "url": "/static/media/icon_GitHub.174d4536.svg"
+  },
+  {
+    "revision": "1adc37635e36f9ad9ad33ac1186c3326",
+    "url": "/static/media/icon_Resume.1adc3763.svg"
+  },
+  {
+    "revision": "1dcc3926050ea85398cb02b002c4772e",
+    "url": "/static/media/icon_arrow.1dcc3926.svg"
+  },
+  {
+    "revision": "49a8c94266763d829e367b3bdcc4e5cd",
+    "url": "/static/media/icon_linkedin.49a8c942.svg"
+  },
+  {
+    "revision": "f74c6404fd209703b91c2183d609456c",
+    "url": "/static/media/icon_view.f74c6404.svg"
+  },
+  {
+    "revision": "86b5e0c020798e152a29a779abc35d87",
+    "url": "/static/media/icon_website.86b5e0c0.svg"
+  },
+  {
+    "revision": "a6730f086319a52dc05d59a9721009ef",
+    "url": "/static/media/logo.a6730f08.svg"
+  }
+]);
